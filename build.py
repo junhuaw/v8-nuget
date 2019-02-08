0d984ab8f16e84af0b617fd791ee32a3ec6a0d8a
Junhua Wang <junhuaw@microsoft.com>
2019-02-08 09:05:21 -0800

    make FORCE_LANG a command option

--- build.py
+++ build.py
@@ -22,12 +22,12 @@ elif V8_VERSION.count('.') < 2 and all(x.isdigit() for x in V8_VERSION.split('.'
 PLATFORM = sys.argv[2] if len(sys.argv) > 2 else os.environ.get('PLATFORM', '')
 PLATFORMS = [PLATFORM] if PLATFORM else ['x86', 'x64']
 
-CONFIGURATION = sys.argv[3] if len(sys.argv) > 3 else os.environ.get('CONFIGURATION', '')
+FORCE_CLANG = (sys.argv[3] if len(sys.argv) > 3 else os.environ.get('FORCE_CLANG')) == '1'
+
+CONFIGURATION = sys.argv[4] if len(sys.argv) > 4 else os.environ.get('CONFIGURATION', '')
 CONFIGURATIONS = [CONFIGURATION] if CONFIGURATION else ['Debug', 'Release']
 
-XP_TOOLSET = (sys.argv[4] if len(sys.argv) > 4 else os.environ.get('XP')) == '1'
-
-FORCE_CLANG = os.environ.get('FORCE_CLANG') == '1'
+XP_TOOLSET = (sys.argv[5] if len(sys.argv) > 5 else os.environ.get('XP')) == '1'
 
 PACKAGES = ['v8', 'v8.redist', 'v8.symbols']
 
